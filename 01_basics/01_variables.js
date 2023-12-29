cff28250be3d4f5dce0b6d548fbaa58bc977618f
onkarlamkane <125243488+onkarlamkane@users.noreply.github.com>
2023-12-29 16:48:38 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const  customerId = 14522
+let email = "onkar@.com"
+var password = "123"
+accountCity = "pune"
+//customerId = 789
+email = "abc@.com"
+password = "456"
+accountCity ="solapur"
+let name
+console.table([customerId,email,password,accountCity,name])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+ //alert (3+3) we are using nodejs, not a browser.
+ //console.log (3
+    +3;//code readability shoud be high
+    //datatypes
+    //number = 2 to power53
+    //bigint
+    //strint = ""
+    //boolean = true/false
+    //null =   special type/it is one type of stand alone value
+    //undefined = no define value/no assingn value
+    //symbol =  unique
+
+    //object
+    //typeof
