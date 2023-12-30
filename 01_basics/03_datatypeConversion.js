3f1110dc3c09bbb6145a1b08a76d626fd527dd79
onkarlamkane <125243488+onkarlamkane@users.noreply.github.com>
2023-12-30 08:18:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,24 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33"=> 33
+// "33abc"  => NaN (Not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "1"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true ; 0 => false
+// "" =>  false
+// "onkar" => true
+
+let no = 33
+let stringno = String(no)
+console.log(stringno);
+console.log(typeof stringno);
+
+
