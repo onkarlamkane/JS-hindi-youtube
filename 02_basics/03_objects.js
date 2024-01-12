ec01f01307677e75638976bf832530e1c31bc593
onkarlamkane <125243488+onkarlamkane@users.noreply.github.com>
2024-01-12 10:02:51 +0000

    object basics

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -10,6 +10,7 @@ const JsUser = {
     name : "onkar",
     age : 23,
     [mySym] : "myKey1", //every time symbol declare in square bracket symbol syntax
+    //use symbol datatype variable name declare in square bracket
     location : "pune",
     email : "onkar@google.com",
     login: false,
@@ -18,7 +19,7 @@ const JsUser = {
     
 }
 // console.log(JsUser.email);
-// console.log(JsUser["email"]);
+// console.log(JsUser["email"]); //access the object variable most use mthod 
 // console.log(JsUser[mySym]);
 JsUser.email = "omkar@gmail.com" 
 //console.log(JsUser);
@@ -28,10 +29,12 @@ JsUser.email = "rahul@gmail.com"
 
 
 JsUser.greeting = function(){
-    console.log("Hello JsUser ");
+    console.log("Hello JsUser "); // greeting is function
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JsUser,${this.name}`);
+    console.log(`Hello JsUser,${this.name}`); // greetingtwo is a function print with object value print by this. keyword
+    // object ke under ke value ko reference karne ke liye
+    //string to converted into backticks called as string intepulation
 }
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
