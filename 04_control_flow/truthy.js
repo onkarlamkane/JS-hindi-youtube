c2068479017333103d4b11e44d8b64d934a9e975
onkarlamkane <125243488+onkarlamkane@users.noreply.github.com>
2024-01-16 10:30:22 +0000

    control flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,9 @@
+// const userEmail = "onkar@gmail.ai"
+// if (userEmail){
+//     console.log("got the user email");
+// }
+// else{
+//     console.log("didn't the user email");
+// }
+
+console.log("hello");
